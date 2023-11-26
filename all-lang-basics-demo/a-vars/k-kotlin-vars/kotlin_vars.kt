e30f450d7732ecdf5c74f64b51416aaeb367830f
danieljsingh <152046213+danieljsingh@users.noreply.github.com>
2023-11-25 19:43:41 -0800

    Create kotlin_vars.kt

---
+++ all-lang-basics-demo/a-vars/k-kotlin-vars/kotlin_vars.kt
@@ -0,0 +1,38 @@
+fun main() {
+    // Numbers
+    val byteNumber: Byte = 42
+    val shortNumber: Short = 1000
+    val intNumber: Int = 123456
+    val longNumber: Long = 9876543210L  // Note the 'L' at the end for Long literals
+    val floatNumber: Float = 3.14f      // Note the 'f' at the end for Float literals
+    val doubleNumber: Double = 2.71828
+
+    // Characters
+    val char: Char = 'A'
+
+    // Booleans
+    val isTrue: Boolean = true
+    val isFalse: Boolean = false
+
+    // Strings
+    val message: String = "Hello, Kotlin!"
+
+
+    // Nullable Types
+    val nullableString: String? = null
+
+
+    // Print values
+    println("Byte: $byteNumber")
+    println("Short: $shortNumber")
+    println("Int: $intNumber")
+    println("Long: $longNumber")
+    println("Float: $floatNumber")
+    println("Double: $doubleNumber")
+    println("Char: $char")
+    println("Boolean (True): $isTrue")
+    println("Boolean (False): $isFalse")
+    println("String: $message")
+    println("Nullable String: $nullableString")
+
+}
